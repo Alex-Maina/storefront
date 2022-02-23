bbd75fd4f13c7cc2beab8b08eb987c3812425a01
Maina Kimani <kalexmaina@gmail.com>
2022-02-23 16:36:06 +0000

    Registering models

--- store/models.py
+++ store/models.py
@@ -1,14 +1,15 @@
 from random import choices
 from django.db import models
 
-
-class Promotion(models.Model):
-    description = models.CharField(max_length=255)
-    discount = models.FloatField()
-
-
+#collections model
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
+    
+    def __str__(self) -> str:
+        return self.title
+
+
 
 # products model
 class Product(models.Model):
@@ -19,7 +20,14 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    #order by title
+    class meta:
+        ordering = ['title',]
+    
     
 #customer model
 class Customer(models.Model):
@@ -38,12 +46,25 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices = MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    #order by title
+    class meta:
+        ordering = ['title',]
+
+
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
-      
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    
 class Order(models.Model):
     PENDING = 'P'
     COMPLETE = 'C'
@@ -58,18 +79,34 @@ class Order(models.Model):
     placed_at =models.DateField(auto_now_add=True)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     
+    def __str__(self) -> str:
+        return self.title
+    
+    
+    
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField ()
     unit_price = models.DecimalField (max_digits=6, decimal_places=2)
     
+    def __str__(self) -> str:
+        return self.title
+    
+    
+    
 class Cart (models.Model):
     created_at= models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
     
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+    
+    def __str__(self) -> str:
+        return self.title
     
     
